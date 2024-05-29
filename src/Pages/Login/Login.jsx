f1b978f96945b82328b50ed2af3209383a814d9f
Your Name <you@example.com>
2024-05-29 17:54:35 +0545

    recaptcha added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,25 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 import Navbar from "../../Components/Navbar/Navbar.jsx";
 import GoogleIcon from "../../assets/GoogleIcon.jsx";
 import { FaRegEyeSlash, FaRegEye } from "react-icons/fa";
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
+  const [captchaLoaded, setCaptchaLoaded] = useState(false);
+
+  useEffect(() => {
+    console.log("Component mounted or updated");
+    setCaptchaLoaded(true);
+  }, []);
 
   const togglePasswordVisibility = () => {
     setShowPassword((prevState) => !prevState);
+  };
+
+  const onChange = (value) => {
+    console.log("Captcha value:", value);
   };
 
   return (
     <>
       <div>
         <Navbar />
-        <div className="mt-60 flex flex-col items-center">
+        <div className="mt-24 flex flex-col items-center">
           <h1 className="text-[32px] font-bold text-[#12235B]">Create Pointrack account</h1>
           <h3 className="text-[18px] text-[#12235B]">Get started for free</h3>
-          <div className="border border-[#162E82] flex items-center justify-center w-[446px] h-[48px] mt-4 mb-4 p-2 rounded-[6px]">
+          <div className="border border-[#162E82] flex items-center justify-center w-[446px] h-[48px] mt-6 mb-4 p-2 rounded-[6px] cursor-pointer hover:bg-gray-100">
             <GoogleIcon />
-            <span className="ml-2">Continue with Google</span>
+            <span className="ml-2 text-[#162E82] font-semibold">Continue with Google</span>
           </div>
           <p>or</p>
 
@@ -55,6 +66,11 @@ export default function Login() {
             <p className="mt-4 text-center text-gray-600">
               By continuing, you agree to the terms of service and privacy policy
             </p>
+            <div className="flex justify-center mt-4">
+              {captchaLoaded && (
+                <ReCAPTCHA sitekey={import.meta.env.VITE_SITEKEY} onChange={onChange} />
+              )}
+            </div>
           </div>
         </div>
       </div>
